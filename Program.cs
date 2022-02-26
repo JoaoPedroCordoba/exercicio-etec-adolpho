95f0772766c276122771e79cf701e9ec2263d957
Anacarolinar21 <anacarolinarodriguescarolina2@gmail.com>
2022-02-25 21:54:33 -0300

    exercicio cidade

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("cidades do litoral:");
+Console.WriteLine("santos");
+Console.WriteLine("mongagua");
+Console.WriteLine("praia grande");
+Console.WriteLine("são Vicente");
+Console.WriteLine("guaruja");
+Console.WriteLine("peruibe");
+Console.WriteLine("intanhaem");
